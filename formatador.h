50c73701a13f555014ae180c9448fac9d9043e39
alicegischewski <alice.nogueira1@estudante.ufla.br>
2026-05-25 16:54:03 -0300

    Add header file formatador.h with function declarations

---
+++ formatador.h
@@ -0,0 +1,14 @@
+#ifndef FORMATADOR_H
+#define FORMATADOR_H
+
+#include <string>
+
+using namespace std;
+
+void mostrarDivisoes(int num, int baseDestino);
+
+void mostrarSomatorio(string num, int baseNum, int baseResultado);
+
+void mostrarParteFracionaria(double valorFracionario, int baseDestino);
+
+#endif

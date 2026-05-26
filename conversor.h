609d836dc528ac2482f1f6cd792368d18c8e1fd2
zvinho <alvarorochapacheco@gmail.com>
2026-05-26 02:05:41 -0300

    Update conversor.h

--- conversor.h
+++ conversor.h
@@ -5,6 +5,11 @@
 #include <string>
 #include <cmath>
 
+struct resultadoFracionario {
+    bool truncado;
+    std::string valor;
+};
+
 std::string conversorDeDecimal(int inteiroNum, int base);
 std::string converterFracionarioParaDecimal(std::string parteFracionaria, int baseOrigem);
 std::string conversorDeBase(std::string num, int baseNum, int baseResultado);
@@ -13,10 +18,11 @@ int transformarChar(char n);
 std::string converterFracionarioParaDecimal(std::string parteFracionaria, int baseOrigem);
 std::string conversorDeDecimal(int num, int baseDestino);
 int baseParaDecimal(std::string num, int baseNum);
+resultadoFracionario converterFracionarioParaBase(double valorFracionario, int baseDestino);
 std::string octalBin(char n);
 std::string hexBin(char n);
 std::string conversorDeBase(std::string num, int baseNum, int baseResultado);
-
+double stringFracionariaParaDouble(std::string strFracao);
 
 
 #endif
